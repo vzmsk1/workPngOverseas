33159bbdbac34e9aaf0caccc4ba7befe2d8ef89c
vyazemskaya <vyazemskayanna@gmail.com>
2024-03-20 12:19:32 +0300

    update

--- src/js/planet.js
+++ src/js/planet.js
@@ -3,6 +3,7 @@ import ThreeGlobe from 'three-globe';
 import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js';
+import { excludedCountries } from './capitals';
 
 document.addEventListener('DOMContentLoaded', function () {
   if (document.getElementById('globeViz')) {
@@ -17,46 +18,10 @@ Object.assign(THREE, { TrackballControls, CSS2DRenderer });
 
 const colorInterpolator = t => `rgba(173,255,0,${1 - t + 0.5})`;
 
-//country array
-const countriesList = [
-  {
-    country: 'Russia',
-    lat: 55.751244,
-    lng: 37.618423,
-  },
-  {
-    country: 'Poland',
-    lat: 52.237049,
-    lng: 21.017532,
-  },
-  {
-    country: 'Japan',
-    lat: 35.652832,
-    lng: 139.839478,
-  },
-  {
-    country: 'India',
-    lat: 20.0,
-    lng: 77.0,
-  },
-];
-
-console.log(location);
-
 function createGlobe() {
   fetch('./ne_110m_admin_0_countries.geojson')
     .then(res => res.json())
     .then(countries => {
-      let countryPoints =
-        countriesList.map(country => ({
-          ...country,
-          maxR: 3,
-          propagationSpeed: 3,
-          repeatPeriod: 300,
-        })) || [];
-
-      console.log(countriesList);
-
       const Globe = new ThreeGlobe({
         waitForGlobeReady: true,
         animateIn: true,
@@ -66,7 +31,6 @@ function createGlobe() {
         // .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
 
         // rings
-        .ringsData(countryPoints)
         .ringColor(() => colorInterpolator)
         .ringMaxRadius('maxR')
         .ringPropagationSpeed('propagationSpeed')
@@ -82,19 +46,31 @@ function createGlobe() {
         .polygonSideColor(() => '#6f6295')
         .polygonStrokeColor(() => '#7f77ce');
 
-      //   const arr = Globe.polygonsData().filter(
-      //     e => e.properties.NAME === 'China'
-      //   );
+      const countriesList = [];
 
-      //   console.log(Globe.ringsData());
+      fetch('./capitals.geojson')
+        .then(res => res.json())
+        .then(arr => {
+          arr.features.forEach(i => {
+            if (!excludedCountries.includes(i.properties.country)) {
+              countriesList.push({
+                country: i.properties.country,
+                lat: i.geometry.coordinates[0],
+                lng: i.geometry.coordinates[1],
+              });
+            }
+          });
 
-      //   Globe.polygonCapColor(e => {
-      //     if (e.properties.NAME === 'China') {
-      //       return '#783ef2';
-      //     } else {
-      //       return '#422f7e';
-      //     }
-      //   });
+          let countryPoints =
+            countriesList.map(country => ({
+              ...country,
+              maxR: 3,
+              propagationSpeed: 3,
+              repeatPeriod: 300,
+            })) || [];
+
+          Globe.ringsData(countryPoints);
+        });
 
       // Setup renderer
       const renderer = new THREE.WebGLRenderer();
