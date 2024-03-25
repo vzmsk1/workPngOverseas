af526e18a5a5fe204a99336af4314a63ba1b60a4
vyazemskaya <vyazemskayanna@gmail.com>
2024-03-25 12:09:47 +0300

    update: header

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,19 +1,28 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-  if (!document.querySelector('.hamburger')) return;
-  const hamburger = document.querySelector('.hamburger');
-  const burgerMenu = document.querySelector('.header__burger-menu');
-  const hamburgerClose = document.querySelector('.header__burger-close');
-  
+  const mm = window.matchMedia("(max-width: 768px)");
+
+  if (!document.querySelector(".hamburger")) return;
+  const hamburger = document.querySelector(".hamburger");
+  const burgerMenu = document.querySelector(".header__burger-menu");
+  const hamburgerClose = document.querySelector(".header__burger-close");
+
   function closeBurgerMenu() {
-    burgerMenu.classList.remove('active');
-  };
-  
+    burgerMenu.classList.remove("active");
+    document.documentElement.classList.remove("_lock");
+  }
+
   function openBurgerMenu() {
-    burgerMenu.classList.add('active');
-  };
-  
-  hamburger.addEventListener('click', openBurgerMenu);
-  
-  hamburgerClose.addEventListener('click', closeBurgerMenu);
+    burgerMenu.classList.add("active");
+    document.documentElement.classList.add("_lock");
+  }
+
+  hamburger.addEventListener("click", openBurgerMenu);
+
+  hamburgerClose.addEventListener("click", closeBurgerMenu);
+
+  mm.addEventListener("change", function () {
+    if (!mm.matches) {
+      closeBurgerMenu();
+    }
+  });
 });
